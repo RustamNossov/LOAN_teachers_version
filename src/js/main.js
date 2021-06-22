24ec2085bc6f38704fcd23e24a31131efa7518a0
RustamNossov <rustamn48@gmail.com>
2021-06-22 12:40:59 +0600

    slides at pages are configurated

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,39 @@
-import Slider from './modules/slider';
+import MainSlider from './modules/slider/slider-main';
 import VideoPlayer from './modules/playVideo';
+import MiniSlider from './modules/slider/slider-mini';
 
 
 window.addEventListener('DOMContentLoaded', ()=> {
-    const slider = new Slider('.page', '.next');
+    const slider = new MainSlider({container:'.page', btns:'.next'});
     slider.render();
 
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
+
+    const showUpSlider = new MiniSlider({
+        container: '.showup__content-slider',
+        next: '.showup__next',
+        prev: '.showup__prev',
+        activeClass: 'card-active',
+        animate: true
+    });
+    showUpSlider.init();
+
+    const modulesSlider = new MiniSlider({
+        container: '.modules__content-slider',
+        next: '.modules__info-btns .slick-next',
+        prev: '.modules__info-btns .slick-prev',
+        activeClass: 'card-active',
+        animate: true
+    });
+    modulesSlider.init();
+
+    const feedSlider = new MiniSlider({
+        container: '.feed__slider-wrapper',
+        next: '.feed__slider .slick-next',
+        prev: '.feed__slider .slick-prev',
+        activeClass: 'feed__item-active'
+        //animate: true
+    });
+    feedSlider.init();
 })
